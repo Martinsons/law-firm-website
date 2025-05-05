a7e6b84fe7287f80dd9cddae9c9c1a205b5f0cf7
Martins Martinsons <martins.martinsons@yahoo.com>
2025-05-05 22:21:09 +0300

    commit

--- src/components/practice/PracticeAreas.tsx
+++ src/components/practice/PracticeAreas.tsx
@@ -248,12 +248,9 @@ export default function PracticeAreasDetail({ locale }: PracticeAreasDetailProps
                       >
                         <svg className="h-5 w-5 text-blue-600 mr-2 mt-0.5 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4" />
-                      <li key={sIndex} className="flex items-start">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-blue-600 mr-2 mt-1" viewBox="0 0 20 20" fill="currentColor">
-                          <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                         </svg>
                         <span className="text-gray-700">{service}</span>
-                      </li>
+                      </motion.li>
                     ))}
                   </ul>
                 </div>
